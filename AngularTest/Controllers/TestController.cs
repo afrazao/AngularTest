8ec0f9beebefe868b84f1bd633e46108e1c8bc85
António Frazão <afrancofrazao@gmail.com>
2019-07-02 21:37:06 +0100

    Project and Test Class/Component Created

---
+++ AngularTest/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        private static string[] DDLTest = new[]
+        {
+            "António", "Matias", "Filips"
+        };
+
+        [HttpGet("action")]
+        public IEnumerable<Names> NameList()
+        {
+            //return Enumerable.Select(index => new Names
+            //{
+            //    Name
+            //});
+        }
+
+        public class Names
+        {
+            public string Name { get; set; }
+        }
+    }
+}
